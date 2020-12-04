39825362a72c72e030aaed11bab3fc5ce9fb2c05
ylu <ylu@ert.com>
2020-12-04 17:50:24 -0500

    integration

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import MarketingApp from './components/MarketingApp'
 
 const App = () => {
   return (
     <div>
-      <h1>Hi, container123</h1>
+      <h1>Hi from container App</h1>
+      <hr />
+      <MarketingApp />
     </div>
   )
 }
