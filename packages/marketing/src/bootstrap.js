a19e82dfafc73ab5cbdd5c3ef05c21670407b5c0
ylu <ylu@ert.com>
2020-12-05 00:22:48 -0500

    done routing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App'
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory()
+
+  if (onNavigate) {
+    // whenever the path change, call onNavigate
+    history.listen(onNavigate)
+  }
+
   ReactDOM.render(
-    <App />, el
+    <App history={history} />, el
   )
+
+  return {
+    onParentNavigate({pathname: nextPathname}) {
+      const { pathname } = history.location
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    }
+  }
 }
 
 // If we are in development and in isolation, 
@@ -14,7 +32,7 @@ const mount = (el) => {
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root')
   if (devRoot) {
-    mount(devRoot)
+    mount(devRoot, { defaultHistory: createBrowserHistory() })
   }
 }
 
