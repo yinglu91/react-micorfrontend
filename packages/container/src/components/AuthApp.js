723198b13ae4b3efdd439f9912f3ce7fde277714
ylu <ylu@ert.com>
2020-12-08 20:44:59 -0500

    container to auth

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import {mount} from 'auth/AuthApp'
 import React, {useRef, useEffect} from 'react'
 import { useHistory } from 'react-router-dom'
 
-const AuthApp = () => {
+const AuthApp = ({ onSignIn }) => {
   const ref = useRef(null)
   const history = useHistory()
 
@@ -15,7 +15,8 @@ const AuthApp = () => {
         if (pathname !== nextPathname) {
           history.push(nextPathname)
         }
-      }
+      },
+      onSignIn
     })
 
     // whenever container change path, call onParentNavigate
