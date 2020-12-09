9cce5fb30c7feb486f6e8c1ceaf0445b5bab6a9d
ylu <ylu@ert.com>
2020-12-08 20:54:09 -0500

    add logout

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -24,7 +24,7 @@ export default () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
+          <Header onSignOut={()=> setIsSignedIn(false)} isSignedIn={isSignedIn} />
           <Suspense fallback={<Progress />}>
             <Switch>
               <Route path="/auth">
